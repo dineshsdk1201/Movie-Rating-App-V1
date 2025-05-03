bc6a2d378c9862965363eedfee17e3badaa0080f
Dinesh <dineshsdk1978@gmail.com>
2025-05-03 12:36:32 +0530

    v1 completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
-import "./index.css";
 
 export default function App() {
-  const [myTip, setMyTip] = useState(0);
-  const [friendTip, setFriendTip] = useState(0);
-  const [bill, setBill] = useState(0);
-
+  const [bill, setBill] = useState("");
+  const [myTip, setMytip] = useState(10);
+  const [ftip, setFtip] = useState(10);
+  function handleReset() {
+    setBill("");
+    setMytip(10);
+    setFtip(10);
+  }
   return (
     <>
       <Bill bill={bill} setBill={setBill} />
-      <MyTip myTip={myTip} setMyTip={setMyTip} />
-      <FriendTip friendTip={friendTip} setFriendTip={setFriendTip} />
-      <TotalBill bill={bill} myTip={myTip} friendTip={friendTip} />
+      <MyTip myTip={myTip} setMytip={setMytip} />
+      <FriendTip ftip={ftip} setFtip={setFtip} />
+      <TotalBill ftip={ftip} bill={bill} myTip={myTip} />
+      <Reset handleReset={handleReset} />
     </>
   );
 }
@@ -19,7 +23,7 @@ export default function App() {
 function Bill({ bill, setBill }) {
   return (
     <div>
-      <p style={{ display: "inline" }}>How Much The Bill</p>
+      <h3 style={{ display: "inline" }}>How Much the bill is?</h3>
       <input
         type="number"
         value={bill}
@@ -28,11 +32,11 @@ function Bill({ bill, setBill }) {
     </div>
   );
 }
-function MyTip({ myTip, setMyTip }) {
+function MyTip({ myTip, setMytip }) {
   return (
     <div>
-      <p style={{ display: "inline" }}>How You Likely to give he tip</p>
-      <select value={myTip} onCanPlay={(e) => setMyTip(e.target.value)}>
+      <h3 style={{ display: "inline" }}>How You are satisfied</h3>
+      <select value={myTip} onChange={(e) => setMytip(e.target.value)}>
         <option value={10}>10%</option>
         <option value={20}>20%</option>
         <option value={30}>30%</option>
@@ -41,11 +45,11 @@ function MyTip({ myTip, setMyTip }) {
   );
 }
 
-function FriendTip({ friendTip, setFriendTip }) {
+function FriendTip({ ftip, setFtip }) {
   return (
     <div>
-      <p style={{ display: "inline" }}>How Your friend Like to share tip</p>
-      <select value={friendTip} onCanPlay={(e) => setFriendTip(e.target.value)}>
+      <h3 style={{ display: "inline" }}>How Your Friend satisfied</h3>
+      <select value={ftip} onChange={(e) => setFtip(e.target.value)}>
         <option value={10}>10%</option>
         <option value={20}>20%</option>
         <option value={30}>30%</option>
@@ -54,13 +58,30 @@ function FriendTip({ friendTip, setFriendTip }) {
   );
 }
 
-function TotalBill({ friendTip, myTip, bill }) {
-  const totalTip = ((myTip + friendTip) / bill) * 100;
-  console.log(totalTip);
+function TotalBill({ ftip, myTip, bill }) {
+  // console.log(myTip, ftip);
+  console.log(typeof myTip, typeof ftip, typeof bill);
+  const totalTipPercentage = (ftip + myTip) / 2;
+  // console.log(totalTipPercentage);
+  const totalTip = (totalTipPercentage / 100) * Number(bill);
+  // console.log(totalTip);
+  const totalBill = totalTip + Number(bill);
+
   return (
     <>
-      <p>The actual Bill is {bill} </p>
-      <p>The total Bill is {totalTip + bill} </p>
+      {bill && (
+        <div>
+          <h3>The total tip is {totalTip}</h3>
+          <h3>Your Total Bill is {totalBill}</h3>
+        </div>
+      )}
+    </>
+  );
+}
+function Reset({ handleReset }) {
+  return (
+    <>
+      <button onClick={handleReset}>reset</button>
     </>
   );
 }
