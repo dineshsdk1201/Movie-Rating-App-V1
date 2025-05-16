75b4d9ed49e8a974f3be8fb27de1f3349ed4ec56
Dinesh <dineshsdk1978@gmail.com>
2025-05-16 10:14:58 +0530

    add useRef select the search element on initial liad

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 import {
   isLabelWithInternallyDisabledControl,
@@ -212,6 +212,22 @@ function Logo() {
 function Search({ query, setQuery }) {
   // const [query, setQuery] = useState("");
 
+  const inputEl = useRef(null);
+  useEffect(() => {
+    function callBack(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callBack);
+    return () => {
+      document.addEventListener("keydown", callBack);
+    };
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -219,6 +235,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
